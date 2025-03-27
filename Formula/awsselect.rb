a9ceb6903fba654f125ce79bd4b1bcb95db2a476
Oscar Angulo <oscar.angulo@medismart.live>
2025-03-27 09:12:22 -0300

    Update awsselect.rb

--- Formula/awsselect.rb
+++ Formula/awsselect.rb
@@ -1,6 +1,6 @@
 class Awsselect < Formula
   desc "Herramienta interactiva para seleccionar y persistir perfiles de AWS CLI"
-  homepage "https://github.com/tuusuario/awsselect"
+  homepage "https://github.com/oscarangulo/awsselect"
   url "https://github.com/oscarangulo/awsselect/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "3252071d9dce3afc5dca0ab672a02b8d43402dd80ac9616ad8970cc707bee937" # lo calculamos abajo
   version "1.0.1"
